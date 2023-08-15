e4b310f496e1982404868ef6fc560eef680e9ef2
Chupa-chups <122631978+Chupa-chups-dev@users.noreply.github.com>
2023-08-15 12:44:32 +0200

    Описание изменений

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
             <ul>
               <li><a href="tel:+74994605810">+7 (499) 460-58-10</a></li>
               <li><a href="mailto:info@carboncopy.ru">info@carboncopy.ru</a></li>
-              <li>Трехпрудный пер. на г. Москва, Большой Гнездниковский пер., д. 1, стр. 2</li>
+              <li>г. Москва, Большой Гнездниковский пер., д. 1, стр. 2</li>
             </ul>
             <img src={logo} alt="logo" />
           </div>
